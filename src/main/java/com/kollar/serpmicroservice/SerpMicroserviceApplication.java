33ba7f2d421c238378910bb878abb158f7aa232a
xkollar3 <xkollar3@fi.muni.cz>
2024-01-06 12:19:51 +0100

    Initial commit

---
+++ src/main/java/com/kollar/serpmicroservice/SerpMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kollar.serpmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerpMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerpMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kollar/serpmicroservice/SerpMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kollar.serpmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerpMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
